63b5d53ba1f12e0a39b0f797052d62ddc0f0fb31
aikhi007 <781089956@qq.com>
2021-12-08 09:56:04 +0800

    s53☆☆滑动窗口

---
+++ s53/src/Solution.java
@@ -0,0 +1,39 @@
+/* https://leetcode-cn.com/problems/maximum-sum-of-3-non-overlapping-subarrays/ */
+public class Solution {
+    public int[] maxSumOfThreeSubarrays(int[] nums, int k) {
+        int[] ans = new int[3];
+        int sum1 = 0, maxSum1 = 0, maxSum1Idx = 0;
+        int sum2 = 0, maxSum12 = 0, maxSum12Idx1 = 0, maxSum12Idx2 = 0;
+        int sum3 = 0, maxTotal = 0;
+        for (int i = 2 * k; i < nums.length; i++) {
+            sum1 += nums[i - 2 * k];
+            sum2 += nums[i - k];
+            sum3 += nums[i];
+            // k = 2
+            // 0 1 2 3 4 5
+            if (i >= 3 * k - 1) {
+                if (sum1 > maxSum1) {
+                    maxSum1 = sum1;
+                    maxSum1Idx = i - 3 * k + 1;
+                }
+                if (maxSum1 + sum2 > maxSum12) {
+                    maxSum12 = maxSum1 + sum2;
+                    maxSum12Idx1 = maxSum1Idx;
+                    maxSum12Idx2 = i - 2 * k + 1;
+                }
+
+                if (maxSum12 + sum3 > maxTotal) {
+                    maxTotal = maxSum12 + sum3;
+                    ans[0] = maxSum12Idx1;
+                    ans[1] = maxSum12Idx2;
+                    ans[2] = i - k + 1;
+                }
+                sum1 -= nums[i - 3 * k + 1];
+                sum2 -= nums[i - 2 * k + 1];
+                sum3 -= nums[i - k + 1];
+            }
+
+        }
+        return ans;
+    }
+}
