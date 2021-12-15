551c8f8fe4c86943bdf584f6670390d76a3d9281
aikhi007 <781089956@qq.com>
2021-12-15 23:54:01 +0800

    s60☆☆喧闹与富有

---
+++ s60/src/Solution.java
@@ -0,0 +1,14 @@
+/* https://leetcode-cn.com/problems/loud-and-rich/ */
+public class Solution {
+    public int[] loudAndRich(int[][] richer, int[] quiet) {
+        int n = quiet.length;
+        int[] r = new int[n];
+        int[] answer = new int[n];
+
+        for (int[] pair : richer) {
+            int rich = pair[0];
+            int pool = pair[1];
+
+        }
+    }
+}
