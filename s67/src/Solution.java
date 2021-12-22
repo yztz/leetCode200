ee12ff5ff12921193a53799e5cb1748beb7a784e
aikhi007 <781089956@qq.com>
2021-12-23 00:02:00 +0800

    s67☆☆KMP todo

--- s67/src/Solution.java
+++ s67/src/Solution.java
@@ -1,11 +1,12 @@
 public class Solution {
     public int repeatedStringMatch(String a, String b) {
-        StringBuilder sb = new StringBuilder(a);
-        int answer = 1;
-        while (2 * b.length() >= sb.length()) {
-            if (sb.indexOf(b) != -1) return answer;
+        StringBuilder sb = new StringBuilder();
+        int ans = 0;
+        int max = 2 * a.length() + b.length();
+        while (sb.length() < max) {
             sb.append(a);
-            answer++;
+            ans++;
+            if (sb.toString().contains(b)) return ans;
         }
         return -1;
     }
