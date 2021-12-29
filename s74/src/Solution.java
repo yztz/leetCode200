c34d5c9067f9df50638f368e66fac71850c155a3
aikhi007 <781089956@qq.com>
2021-12-29 23:56:51 +0800

    s74☆统计特殊四元组

---
+++ s74/src/Solution.java
@@ -0,0 +1,20 @@
+/* https://leetcode-cn.com/problems/count-special-quadruplets */
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int countQuadruplets(int[] nums) {
+        int n = nums.length;
+        int ans = 0;
+        Map<Integer, Integer> cnt = new HashMap<Integer, Integer>();
+        for (int b = n - 3; b >= 1; --b) {
+            for (int d = b + 2; d < n; ++d) {
+                cnt.put(nums[d] - nums[b + 1], cnt.getOrDefault(nums[d] - nums[b + 1], 0) + 1);
+            }
+            for (int a = 0; a < b; ++a) {
+                ans += cnt.getOrDefault(nums[a] + nums[b], 0);
+            }
+        }
+        return ans;
+    }
+}
