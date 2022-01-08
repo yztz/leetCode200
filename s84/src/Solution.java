3879d488192511aa0433e74b4560cbdb419f5f67
aikhi007 <781089956@qq.com>
2022-01-08 23:05:54 +0800

    s84☆格雷码

---
+++ s84/src/Solution.java
@@ -0,0 +1,17 @@
+/* https://leetcode-cn.com/problems/gray-code/ */
+import java.util.ArrayList;
+import java.util.List;
+
+public class Solution {
+    public List<Integer> grayCode(int n) {
+        List<Integer> ret = new ArrayList<Integer>();
+        ret.add(0);
+        for (int i = 1; i <= n; i++) {
+            int m = ret.size();
+            for (int j = m - 1; j >= 0; j--) {
+                ret.add(ret.get(j) | (1 << (i - 1)));
+            }
+        }
+        return ret;
+    }
+}
