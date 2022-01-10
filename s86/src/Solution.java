42e57b02aa7566be43d6275fba5ce518db5fb240
aikhi007 <781089956@qq.com>
2022-01-10 23:59:05 +0800

    s86☆☆累加数

---
+++ s86/src/Solution.java
@@ -0,0 +1,42 @@
+/* https://leetcode-cn.com/problems/additive-number/ */
+public class Solution {
+    public boolean isAdditiveNumber(String num) {
+
+        int a = 0;
+        for (int i = 0; i < num.length() - 1; i++) {
+            a *= 10;
+            a += num.charAt(i) - '0';
+            int b = 0;
+            for (int j = i + 1; j < num.length(); j++) {
+                b *= 10;
+                b += num.charAt(j) - '0';
+                if (dfs(a, b, num.substring(j + 1))) {
+                    System.out.println(a + " " +b + num.substring(j + 1));
+                    return true;
+                }
+                if (num.charAt(i + 1) == '0') break;
+            }
+        }
+        return false;
+    }
+
+
+    public boolean dfs(int pa, int pb, String num) {
+        if (num.length() == 0) return false;
+        int a = 0;
+        int target = pa + pb;
+        for (int i = 0; i < num.length(); i++) {
+            a *= 10;
+            a += num.charAt(i) - '0';
+            if (a == target) {
+                if (i == num.length() - 1) return true;
+                return dfs(pb, target, num.substring(i + 1));
+            } else if (a > target) {
+                return false;
+            }
+            if (num.charAt(0) == '0') return false;
+        }
+        return false;
+    }
+
+}
