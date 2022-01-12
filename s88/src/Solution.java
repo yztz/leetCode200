443acc232a5065c78697da1474e98df9d1ed209c
aikhi007 <781089956@qq.com>
2022-01-13 00:21:28 +0800

    s88☆☆双向遍历|贪心

---
+++ s88/src/Solution.java
@@ -0,0 +1,21 @@
+/* https://leetcode-cn.com/problems/increasing-triplet-subsequence/ */
+public class Solution {
+    public boolean increasingTriplet(int[] nums) {
+        int n = nums.length;
+        if (n < 3) {
+            return false;
+        }
+        int first = nums[0], second = Integer.MAX_VALUE;
+        for (int i = 1; i < n; i++) {
+            int num = nums[i];
+            if (num > second) {
+                return true;
+            } else if (num > first) {
+                second = num;
+            } else {
+                first = num;
+            }
+        }
+        return false;
+    }
+}
