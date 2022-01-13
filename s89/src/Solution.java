20ba42a5c7bf32bc0f303d461e86c8d3ab45f60b
aikhi007 <781089956@qq.com>
2022-01-13 21:20:33 +0800

    s89☆至少是其他数字两倍的最大数

---
+++ s89/src/Solution.java
@@ -0,0 +1,17 @@
+/* https://leetcode-cn.com/problems/largest-number-at-least-twice-of-others/ */
+public class Solution {
+    public int dominantIndex(int[] nums) {
+        int m1 = -1, m2 = -1;
+        int index = -1;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > m1) {
+                m2 = m1;
+                m1 = nums[i];
+                index = i;
+            } else if (nums[i] > m2) {
+                m2 = nums[i];
+            }
+        }
+        return m1 >= m2 * 2 ? index : -1;
+    }
+}
