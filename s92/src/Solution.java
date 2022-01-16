93ea2e1751a0008ac61059f91bc3893103aade58
aikhi007 <781089956@qq.com>
2022-01-16 19:05:13 +0800

    s92☆☆☆水塘抽样

---
+++ s92/src/Solution.java
@@ -0,0 +1,31 @@
+import java.util.Random;
+
+/* https://leetcode-cn.com/problems/linked-list-random-node/ */
+public class Solution {
+    ListNode head;
+    Random random;
+
+    public Solution(ListNode head) {
+        this.head = head;
+        random = new Random();
+    }
+
+    public int getRandom() {
+        int i = 1, ans = 0;
+        for (ListNode node = head; node != null; node = node.next) {
+            if (random.nextInt(i) == 0) { // 1/i 的概率选中（替换为答案）
+                ans = node.val;
+            }
+            ++i;
+        }
+        return ans;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
