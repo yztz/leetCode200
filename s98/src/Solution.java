557f12f3e9694977d8e01e03156b95b476a21957
aikhi007 <781089956@qq.com>
2022-01-22 14:33:43 +0800

    s98☆限定字符的回文子串删除|脑筋急转弯

---
+++ s98/src/Solution.java
@@ -0,0 +1,13 @@
+/* https://leetcode-cn.com/problems/remove-palindromic-subsequences/ */
+
+public class Solution {
+    public int removePalindromeSub(String s) {
+        int n = s.length();
+        for (int i = 0; i < n / 2; ++i) {
+            if (s.charAt(i) != s.charAt(n - 1 - i)) {
+                return 2;
+            }
+        }
+        return 1;
+    }
+}
